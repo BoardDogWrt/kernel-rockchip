7a01b4e89d23a4d9272dbc066f637adc3e93e83d
Wu Liang feng <wulf@rock-chips.com>
2016-05-16 18:59:40 +0800

    usb: dwc3: add a quirk xhci_slow_suspend_quirk
    On some xHCI controllers (e.g. Rockchip SoCs), which are
    integrated in DWC3 IP, need an extraordinary delay to wait
    for xHCI enter the Halted state(i.e. HCH in the USBSTS
    register is '1'), especially if DWC3 is in DRD mode.
    
    Change-Id: I67c84d4768df95f7616d6716a77cf743e4334122
    Signed-off-by: Wu Liang feng <wulf@rock-chips.com>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -102,6 +102,9 @@ int dwc3_host_init(struct dwc3 *dwc)
 	if (dwc->usb2_lpm_disable)
 		props[prop_idx++].name = "usb2-lpm-disable";
 
+	if (dwc->xhci_slow_suspend_quirk)
+		props[prop_idx++].name = "xhci-slow-suspend";
+
 	/**
 	 * WORKAROUND: dwc3 revisions <=3.00a have a limitation
 	 * where Port Disable command doesn't work.
