dc540d8e1557239889676eeb95793f77aec9becc
Ulf Hansson <ulf.hansson@linaro.org>
2017-01-25 10:12:10 +0100

    mmc: core: Remove redundant code in mmc_set_signal_voltage()
    commit a44efa4796249c6d4341935e90e9105d6e1a5f15 upstream.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Shawn Lin <shawn.lin@rock-chips.com>
    Tested-by: Jan Glauber <jglauber@cavium.com>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1599,13 +1599,6 @@ int mmc_set_signal_voltage(struct mmc_host *host, int signal_voltage, u32 ocr)
 	BUG_ON(!host);
 
 	/*
-	 * Send CMD11 only if the request is to switch the card to
-	 * 1.8V signalling.
-	 */
-	if (signal_voltage == MMC_SIGNAL_VOLTAGE_330)
-		return __mmc_set_signal_voltage(host, signal_voltage);
-
-	/*
 	 * If we cannot switch voltages, return failure so the caller
 	 * can continue without UHS mode
 	 */
