8eabbbe2e19556db1cc9cae8da07fd601e44b3c9
William Wu <william.wu@rock-chips.com>
2019-01-04 11:30:20 +0800

    usb: dwc3: add a new xhci trb ent quirk for xHCI
    commit 8224b79008c1bcc349e2d565c24e11aa78672ea8 rockchip.
    
    On some xHCI controllers (e.g. Rockchip RK3399/RK3328/RK1808),
    which are integrated in DWC3 IP, need to enable the Evaluate
    Next TRB(ENT) flag in the TRB data structure to force xHC to
    pre-fetch the next TRB of a TD. It's useful for the stability
    of xHCI when transfer large data.
    
    Change-Id: I87b1d8b8b6912d77b988362f2f6dcd7766da8b0e
    Signed-off-by: William Wu <william.wu@rock-chips.com>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -43,7 +43,7 @@ out:
 
 int dwc3_host_init(struct dwc3 *dwc)
 {
-	struct property_entry	props[4];
+	struct property_entry	props[5];
 	struct platform_device	*xhci;
 	int			ret, irq;
 	struct resource		*res;
@@ -104,6 +104,9 @@ int dwc3_host_init(struct dwc3 *dwc)
 
 	if (dwc->xhci_slow_suspend_quirk)
 		props[prop_idx++].name = "xhci-slow-suspend";
+
+	if (dwc->xhci_trb_ent_quirk)
+		props[prop_idx++].name = "xhci-trb-ent-quirk";
 
 	/**
 	 * WORKAROUND: dwc3 revisions <=3.00a have a limitation
