2bf32bdd79a17aa70e4794b119bc032a429166f4
jensen <jensenhuang@friendlyarm.com>
2021-09-08 15:43:46 +0800

    drm/panel: lcds: Add support for LC116LF1L02
    Signed-off-by: jensen <jensenhuang@friendlyarm.com>

--- drivers/gpu/drm/panel/panel-friendlyelec.c
+++ drivers/gpu/drm/panel/panel-friendlyelec.c
@@ -224,6 +224,7 @@ static struct {
 	{ "G101E",	&hd101,	         0  },
 	{ "079L1JY41",	&hd079,	         0  },
 	{ "LC116LF1L01",&k116e,	CTP_FT5526_KR },
+	{ "LC116LF1L02",&k116e,	CTP_FT5526_KR },
 
 	{ "EDID",	NULL,	CTP_AUTO    },
 };
