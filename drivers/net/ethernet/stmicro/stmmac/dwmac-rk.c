6d1904ebf10cae2f4779fef5b5fc27023b032058
hmz007 <hmz007@gmail.com>
2021-11-01 18:57:27 +0800

    Revert "net: stmmac: dwmac-rk: Fix ethernet on rk3399 based devices"
    This reverts commit aec3f415f7244b7747a7952596971adb0df2f568.
    
    Signed-off-by: hmz007 <hmz007@gmail.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
@@ -21,7 +21,6 @@
 #include <linux/delay.h>
 #include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
-#include <linux/pm_runtime.h>
 
 #include "stmmac_platform.h"
 
@@ -1529,8 +1528,6 @@ static int rk_gmac_powerup(struct rk_priv_data *bsp_priv)
 		return ret;
 	}
 
-	pm_runtime_get_sync(dev);
-
 	if (bsp_priv->integrated_phy)
 		rk_gmac_integrated_phy_powerup(bsp_priv);
 
@@ -1541,8 +1538,6 @@ static void rk_gmac_powerdown(struct rk_priv_data *gmac)
 {
 	if (gmac->integrated_phy)
 		rk_gmac_integrated_phy_powerdown(gmac);
-
-	pm_runtime_put_sync(&gmac->pdev->dev);
 
 	phy_power_on(gmac, false);
 	gmac_clk_enable(gmac, false);
