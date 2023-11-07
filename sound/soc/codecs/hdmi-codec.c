30fdadf4bd9dc295e9b9cc3d4a69c8994cf0068e
jensen <jensenhuang@friendlyarm.com>
2023-11-07 13:50:32 +0800

    Revert "UPSTREAM: ASoC: hdmi-codec: Add RX support"
    Fixes following error on rk3399 while pulseaudio startup:
      hdmi-audio-codec.3.auto: Only one simultaneous stream supported!
    
    This reverts commit ce1f3e5df09a55114175032d5aabb6ecb47b6a6d.

--- sound/soc/codecs/hdmi-codec.c
+++ sound/soc/codecs/hdmi-codec.c
@@ -300,7 +300,6 @@ struct hdmi_codec_priv {
 
 static const struct snd_soc_dapm_widget hdmi_widgets[] = {
 	SND_SOC_DAPM_OUTPUT("TX"),
-	SND_SOC_DAPM_OUTPUT("RX"),
 };
 
 enum {
@@ -456,7 +455,6 @@ static int hdmi_codec_startup(struct snd_pcm_substream *substream,
 			      struct snd_soc_dai *dai)
 {
 	struct hdmi_codec_priv *hcp = snd_soc_dai_get_drvdata(dai);
-	bool tx = substream->stream == SNDRV_PCM_STREAM_PLAYBACK;
 	int ret = 0;
 
 	dev_dbg(dai->dev, "%s()\n", __func__);
@@ -475,7 +473,7 @@ static int hdmi_codec_startup(struct snd_pcm_substream *substream,
 		}
 	}
 
-	if (tx && hcp->hcd.ops->get_eld) {
+	if (hcp->hcd.ops->get_eld) {
 		ret = hcp->hcd.ops->get_eld(dai->dev->parent, hcp->hcd.data,
 					    hcp->eld, sizeof(hcp->eld));
 
@@ -750,19 +748,14 @@ static int hdmi_codec_pcm_new(struct snd_soc_pcm_runtime *rtd,
 static int hdmi_dai_probe(struct snd_soc_dai *dai)
 {
 	struct snd_soc_dapm_context *dapm;
-	struct snd_soc_dapm_route route[] = {
-		{
-			.sink = "TX",
-			.source = dai->driver->playback.stream_name,
-		},
-		{
-			.sink = dai->driver->capture.stream_name,
-			.source = "RX",
-		},
+	struct snd_soc_dapm_route route = {
+		.sink = "TX",
+		.source = dai->driver->playback.stream_name,
 	};
 
 	dapm = snd_soc_component_get_dapm(dai->component);
-	return snd_soc_dapm_add_routes(dapm, route, 2);
+
+	return snd_soc_dapm_add_routes(dapm, &route, 1);
 }
 
 static void hdmi_codec_jack_report(struct hdmi_codec_priv *hcp,
@@ -853,14 +846,6 @@ static const struct snd_soc_dai_driver hdmi_i2s_dai = {
 		.formats = I2S_FORMATS,
 		.sig_bits = 24,
 	},
-	.capture = {
-		.stream_name = "Capture",
-		.channels_min = 2,
-		.channels_max = 8,
-		.rates = HDMI_RATES,
-		.formats = I2S_FORMATS,
-		.sig_bits = 24,
-	},
 	.ops = &hdmi_dai_ops,
 	.pcm_new = hdmi_codec_pcm_new,
 };
@@ -871,13 +856,6 @@ static const struct snd_soc_dai_driver hdmi_spdif_dai = {
 	.probe = hdmi_dai_probe,
 	.playback = {
 		.stream_name = "SPDIF Playback",
-		.channels_min = 2,
-		.channels_max = 2,
-		.rates = HDMI_RATES,
-		.formats = SPDIF_FORMATS,
-	},
-	.capture = {
-		.stream_name = "Capture",
 		.channels_min = 2,
 		.channels_max = 2,
 		.rates = HDMI_RATES,
