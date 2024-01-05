72d33a03b2644f63732179d2e4772e57b52a2387
Shawn Lin <shawn.lin@rock-chips.com>
2024-01-05 09:46:28 +0800

    PCI: rockchip: dw: Fix NULL pointer of hot_rst_work
    If the hot reset was happening just before the hot_rst_work
    was created, NULL pointer dereference was hit.
    
    [    2.366623] Unable to handle kernel NULL pointer dereference at virtual address 0000000000000100
    [    2.367402] Mem abort info:
    
    ...8<...
    
    [    2.380736] Call trace:
    [    2.380960]  __queue_work+0x30/0x5a0
    [    2.381282]  queue_work_on+0x88/0xa4
    [    2.381607]  rk_pcie_sys_irq_handler+0x1ec/0x280
    [    2.382019]  __handle_irq_event_percpu+0x80/0x250
    [    2.382440]  handle_irq_event+0x64/0x120
    [    2.382796]  handle_fasteoi_irq+0xc8/0x204
    [    2.383162]  __handle_domain_irq+0x78/0xe0
    [    2.383528]  gic_handle_irq+0xf8/0x364
    
    Fix it by masking all misc interrupts before registering rk_pcie_sys_irq_handler.
    And later, they will be unmasked already.
    
    This also revert commit ff22268ee58b
      ("Revert "PCIe: dw: rockchip: Delaying the link training after hot reset"")
    
    Fixes: 679557456b2d ("PCIe: dw: rockchip: Delaying the link training after hot reset")
    Signed-off-by: Shawn Lin <shawn.lin@rock-chips.com>
    Signed-off-by: jensen <jensenhuang@friendlyarm.com>

--- drivers/pci/controller/dwc/pcie-dw-rockchip.c
+++ drivers/pci/controller/dwc/pcie-dw-rockchip.c
@@ -83,6 +83,10 @@
 #define PME_TURN_OFF			(BIT(4) | BIT(20))
 #define PCIE_CLIENT_GENERAL_DEBUG	0x104
 #define PCIE_CLIENT_HOT_RESET_CTRL	0x180
+#define PCIE_LTSSM_APP_DLY1_EN		BIT(0)
+#define PCIE_LTSSM_APP_DLY2_EN		BIT(1)
+#define PCIE_LTSSM_APP_DLY1_DONE	BIT(2)
+#define PCIE_LTSSM_APP_DLY2_DONE	BIT(3)
 #define PCIE_LTSSM_ENABLE_ENHANCE	BIT(4)
 #define PCIE_CLIENT_LTSSM_STATUS	0x300
 #define SMLH_LINKUP			BIT(16)
@@ -102,6 +106,7 @@
 
 #define PCIE_PL_ORDER_RULE_CTRL_OFF	0x8B4
 #define RK_PCIE_L2_TMOUT_US		5000
+#define RK_PCIE_HOTRESET_TMOUT_US	10000
 #define PORT_LOGIC_LTSSM_STATE_L2(ltssm) \
 	((ltssm & PORT_LOGIC_LTSSM_STATE_MASK) == 0x15)
 #define RK_PCIE_ENUM_HW_RETRYIES	2
@@ -143,6 +148,8 @@ struct rk_pcie {
 	u32				l1ss_ctl1;
 	struct dentry			*debugfs;
 	u32				msi_vector_num;
+	struct workqueue_struct		*hot_rst_wq;
+	struct work_struct		hot_rst_work;
 	u32				comp_prst[2];
 	u32				intx;
 };
@@ -1319,13 +1326,37 @@ static void rk_pcie_config_dma_dwc(struct dma_table *table)
 	table->start.chnl = table->chn;
 }
 
+static void rk_pcie_hot_rst_work(struct work_struct *work)
+{
+	struct rk_pcie *rk_pcie = container_of(work, struct rk_pcie, hot_rst_work);
+	u32 val, status;
+	int ret;
+
+	/* Setup command register */
+	val = dw_pcie_readl_dbi(rk_pcie->pci, PCI_COMMAND);
+	val &= 0xffff0000;
+	val |= PCI_COMMAND_IO | PCI_COMMAND_MEMORY |
+		PCI_COMMAND_MASTER | PCI_COMMAND_SERR;
+	dw_pcie_writel_dbi(rk_pcie->pci, PCI_COMMAND, val);
+
+	if (rk_pcie_readl_apb(rk_pcie, PCIE_CLIENT_HOT_RESET_CTRL) & PCIE_LTSSM_APP_DLY2_EN) {
+		ret = readl_poll_timeout(rk_pcie->apb_base + PCIE_CLIENT_LTSSM_STATUS,
+			 status, ((status & 0x3F) == 0), 100, RK_PCIE_HOTRESET_TMOUT_US);
+		if (ret)
+			dev_err(rk_pcie->pci->dev, "wait for detect quiet failed!\n");
+
+		rk_pcie_writel_apb(rk_pcie, PCIE_CLIENT_HOT_RESET_CTRL,
+			(PCIE_LTSSM_APP_DLY2_DONE) | ((PCIE_LTSSM_APP_DLY2_DONE) << 16));
+	}
+}
+
 static irqreturn_t rk_pcie_sys_irq_handler(int irq, void *arg)
 {
 	struct rk_pcie *rk_pcie = arg;
 	u32 chn;
 	union int_status status;
 	union int_clear clears;
-	u32 reg, val;
+	u32 reg;
 
 	status.asdword = dw_pcie_readl_dbi(rk_pcie->pci, PCIE_DMA_OFFSET +
 					   PCIE_DMA_WR_INT_STATUS);
@@ -1366,14 +1397,8 @@ static irqreturn_t rk_pcie_sys_irq_handler(int irq, void *arg)
 	}
 
 	reg = rk_pcie_readl_apb(rk_pcie, PCIE_CLIENT_INTR_STATUS_MISC);
-	if (reg & BIT(2)) {
-		/* Setup command register */
-		val = dw_pcie_readl_dbi(rk_pcie->pci, PCI_COMMAND);
-		val &= 0xffff0000;
-		val |= PCI_COMMAND_IO | PCI_COMMAND_MEMORY |
-		       PCI_COMMAND_MASTER | PCI_COMMAND_SERR;
-		dw_pcie_writel_dbi(rk_pcie->pci, PCI_COMMAND, val);
-	}
+	if (reg & BIT(2))
+		queue_work(rk_pcie->hot_rst_wq, &rk_pcie->hot_rst_work);
 
 	rk_pcie_writel_apb(rk_pcie, PCIE_CLIENT_INTR_STATUS_MISC, reg);
 
@@ -1455,7 +1480,8 @@ static void rk_pcie_fast_link_setup(struct rk_pcie *rk_pcie)
 
 	/* LTSSM EN ctrl mode */
 	val = rk_pcie_readl_apb(rk_pcie, PCIE_CLIENT_HOT_RESET_CTRL);
-	val |= PCIE_LTSSM_ENABLE_ENHANCE | (PCIE_LTSSM_ENABLE_ENHANCE << 16);
+	val |= (PCIE_LTSSM_ENABLE_ENHANCE | PCIE_LTSSM_APP_DLY2_EN)
+		| ((PCIE_LTSSM_APP_DLY2_EN | PCIE_LTSSM_ENABLE_ENHANCE) << 16);
 	rk_pcie_writel_apb(rk_pcie, PCIE_CLIENT_HOT_RESET_CTRL, val);
 }
 
@@ -1841,6 +1867,14 @@ retry_regulator:
 
 	reset_control_deassert(rk_pcie->rsts);
 
+	/*
+	 * Misc interrupts was masked by default. However, they will be
+	 * unmasked by FW before jumpping into kernel. Mask all misc interrupts,
+	 * as we don't need to ack them before registering irq. And they will be
+	 * unmasked later.
+	 */
+	rk_pcie_writel_apb(rk_pcie, PCIE_CLIENT_INTR_MASK, 0xffffffff);
+
 	ret = rk_pcie_request_sys_irq(rk_pcie, pdev);
 	if (ret) {
 		dev_err(dev, "pcie irq init failed\n");
@@ -1911,6 +1945,14 @@ retry_regulator:
 	if (device_property_read_bool(dev, "rockchip,skip-scan-in-resume"))
 		rk_pcie->skip_scan_in_resume = true;
 
+	rk_pcie->hot_rst_wq = create_singlethread_workqueue("rk_pcie_hot_rst_wq");
+	if (!rk_pcie->hot_rst_wq) {
+		dev_err(dev, "failed to create hot_rst workqueue\n");
+		ret = -ENOMEM;
+		goto remove_irq_domain;
+	}
+	INIT_WORK(&rk_pcie->hot_rst_work, rk_pcie_hot_rst_work);
+
 	switch (rk_pcie->mode) {
 	case DW_PCIE_RC_TYPE:
 		ret = rk_add_pcie_port(rk_pcie, pdev);
@@ -1926,12 +1968,12 @@ retry_regulator:
 		return 0;
 
 	if (ret)
-		goto remove_irq_domain;
+		goto remove_rst_wq;
 
 	ret = rk_pcie_init_dma_trx(rk_pcie);
 	if (ret) {
 		dev_err(dev, "failed to add dma extension\n");
-		goto remove_irq_domain;
+		goto remove_rst_wq;
 	}
 
 	if (rk_pcie->dma_obj) {
@@ -1964,6 +2006,8 @@ retry_regulator:
 
 	return 0;
 
+remove_rst_wq:
+	destroy_workqueue(rk_pcie->hot_rst_wq);
 remove_irq_domain:
 	if (rk_pcie->irq_domain)
 		irq_domain_remove(rk_pcie->irq_domain);
